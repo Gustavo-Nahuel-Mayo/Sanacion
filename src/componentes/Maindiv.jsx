53ebbf1f0b7d31ab72124350130be4b7436f5b9e
Gustavo Mayo <94874338+Gustavo-Nahuel-Mayo@users.noreply.github.com>
2023-04-22 18:25:03 -0300

    meta

--- src/componentes/Maindiv.jsx
+++ src/componentes/Maindiv.jsx
@@ -3,7 +3,7 @@ function Maindiv() {
     return(
 
 <div id="main-div">
-    <h2 id="h2-1">¿Cansado del estrés y la ansiedad?---</h2>
+    <h2 id="h2-1">¿Cansado del estrés y la ansiedad?</h2>
         <h2 id="h2-2">¡Descubre la solución con nuestro curso online de Yoga Medicina para el Espíritu!</h2>
             <p className="ideario">Aprende a crear tu propio ecosistema de paz y equilibrio mientras mejoras tu salud física, emocional y espiritual. ¡Únete ahora y fortalece tu cuerpo, mente y espíritu!</p>
             <h1 className="cursoonline">Yoga Medicina para el Espíritu</h1>
